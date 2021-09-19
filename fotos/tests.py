3846d48d818b9ffb76a4f1e4eac5b6f93dac8190
Toygar Aksoy <toygar.aksoy@gmail.com>
2021-09-19 04:03:16 +0300

    Basic homepage tests added

--- fotos/tests.py
+++ fotos/tests.py
@@ -1,8 +1,22 @@
 from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
+
+from fotos.views import home
 
 
 # Create your tests here.
-class SmokeTest(TestCase):
+class HomeTest(TestCase):
 
-    def test_bad(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_resolves_home(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home)
+
+    def test_is_page_correct(self):
+        request = HttpRequest()
+        response = home(request)
+        html = response.content.decode('utf8')
+
+        self.assertTrue(html.startswith('<!DOCTYPE html>'))
+        self.assertIn("<title>Fotos</title>", html)
+        self.assertTrue(html.endswith('</html>'))
